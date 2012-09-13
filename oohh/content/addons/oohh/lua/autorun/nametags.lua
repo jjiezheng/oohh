294c3cfeb48f6ad9384cfd0dbea8a16c730d9ade
Elias Hogstvedt <eliashogstvedt@gmail.com>
2012-09-13 13:21:54 +0200

    improved nametags

--- oohh/content/addons/oohh/lua/autorun/nametags.lua
+++ oohh/content/addons/oohh/lua/autorun/nametags.lua
@@ -1,12 +1,15 @@
+local DISTANCE = 15
 hook.Add("PostDrawMenu", "3d2dtest", function()
 	for key, ent in pairs(entities.GetAll()) do
-		local wpos = ent.GetEyePos and ent:GetEyePos() or ent:GetPos()
-		local pos, vis = wpos:ToScreen(nil, nil, nil, nil, math.rad(90))
-		if vis > 0 then
-			if ent.GetNickname then
-				graphics.DrawText(ent:GetNickname(), pos)
-			else
-				--graphics.DrawText(ent:GetName(), pos)
+		if ent.GetNickname then
+			local wpos = ent.GetEyePos and ent:GetEyePos() or ent:GetPos()
+			wpos = wpos + Vec3(0,0,0.4)
+			local dist = -(entities.GetLocalPlayer():GetEyePos() - wpos):GetLength() + DISTANCE
+			dist = math.clamp(dist, 0, DISTANCE) / DISTANCE
+			dist = dist ^ 2
+			local pos, vis = wpos:ToScreen(nil, nil, nil, nil, math.rad(90))
+			if vis > 0 and dist > 0 then
+				graphics.DrawText(ent:GetNickname(), pos, nil, nil, Color(1,1,1, dist), Vec2(-0.5, 0, 0))
 			end
 		end
 	end
