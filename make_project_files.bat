1fea8edfbe2eb6d2a8c14aa7c5d569ffc289554f
Elias Hogstvedt <eliashogstvedt@gmail.com>
2012-09-24 02:02:20 +0200

    removed Crysis Wars from message

--- make_project_files.bat
+++ make_project_files.bat
@@ -4,14 +4,14 @@ goto again
 
 :again
 	cls
-	echo enter your Crysis Wars or Cryengine3 folder
-	set /P CRYENGINETHREEFOLDER="Crysis Wars or Cryengine3 folder: "
+	echo enter your Cryengine 3 free SDK folder
+	set /P CRYENGINETHREEFOLDER="Cryengine 3 free SDK folder: "
 	if exist "%CRYENGINETHREEFOLDER%\Bin32\CryAction.dll" goto link
 	if not exist "%CRYENGINETHREEFOLDER%\Bin32\CryAction.dll" goto notexist
 
 :notexist
 	cls
-	echo "%CRYENGINETHREEFOLDER%" is not the Crysis Wars or Cryengine3 directory
+	echo "%CRYENGINETHREEFOLDER%" is not the Cryengine 3 free SDK directory
 	echo ( it checks if "%CRYENGINETHREEFOLDER%\Bin32\CryAction.dll" exists )
 	pause
 	goto again
